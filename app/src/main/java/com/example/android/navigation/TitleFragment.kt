153c9b1cce2d7731abae6900684d9f946da59003
ILYAS KERBAL <kerbalsc@gmail.com>
2022-03-20 14:55:43 +0100

    Created navigation resource and linked title fragment with game fragment

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 
@@ -16,6 +18,11 @@ class TitleFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding : FragmentTitleBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_title, container, false)
+//        binding.playButton.setOnClickListener {
+//            //Navigation.findNavController(it).navigate(R.id.action_titleFragment_to_gameFragment)
+//            view?.findNavController()?.navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+        binding.playButton.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment))
         return binding.root
     }
 }
